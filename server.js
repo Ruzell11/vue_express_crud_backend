c18d7f90632dd3723452e1fe58a12743f992d995
Ruzell11 <johnruzell123@gmail.com>
2023-07-22 12:37:33 +0800

    add cors

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 import express from 'express';
 import routes from './src/routes'
+import cors from 'cors'
 const app = express();
 const port = 3000;
 
 // Middleware to parse JSON requests
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(
+    cors({
+        origin: "*",
+        credentials: true,
+    })
+);
 
 app.use('/api', routes)
 // Start the server
